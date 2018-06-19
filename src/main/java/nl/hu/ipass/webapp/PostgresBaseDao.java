3fd68d47d411d845aaf270f4a69bc76d783ea6fc
Julian <julian-buursink@hotmail.com>
2018-06-19 14:41:32 +0200

    heroku

--- src/main/java/nl/hu/ipass/webapp/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/webapp/PostgresBaseDao.java
@@ -1,15 +1,24 @@
 package nl.hu.ipass.webapp;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class PostgresBaseDao {
 	protected final Connection getConnection() {
 		Connection result = null;
 
 		try {
-			Class.forName("org.postgresql.Driver");
-			result = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ipass", "postgres", "postgres");
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+
+			result = ds.getConnection();
+
+			// Class.forName("org.postgresql.Driver");
+			// result =
+			// DriverManager.getConnection("jdbc:postgresql://localhost:5432/ipass",
+			// "postgres", "postgres");
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
