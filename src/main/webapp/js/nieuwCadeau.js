ff86c099f1dc1952309b046f92a4582731f4cbbc
JBuursink <31792727+JBuursink@users.noreply.github.com>
2018-06-19 18:40:05 +0200

    Update nieuwCadeau.js

--- src/main/webapp/js/nieuwCadeau.js
+++ src/main/webapp/js/nieuwCadeau.js
@@ -1,7 +1,7 @@
 document.querySelector("#nieuwCadeauForm").addEventListener("submit",function(){
         var formData = new FormData(document.querySelector("#nieuwCadeauForm"));
         var encData = new URLSearchParams(formData.entries());
-        fetch("/webapp/restservices/planners", {method: 'POST', body: encData})
+        fetch("/restservices/planners", {method: 'POST', body: encData})
         .then(response => response.json())
         .then(function () {
             alert(myJson);
