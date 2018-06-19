279abc6b99c997cdf8fe9ecf3c365412a8c88261
Julian <julian-buursink@hotmail.com>
2018-06-19 16:15:40 +0200

    Boi

--- src/main/java/nl/hu/ipass/webapp/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/webapp/PostgresBaseDao.java
@@ -1,28 +1,78 @@
+//package nl.hu.ipass.webapp;
+//
+//import java.sql.Connection;
+//
+//import javax.naming.InitialContext;
+//import javax.sql.DataSource;
+//
+//public class PostgresBaseDao {
+//	protected final Connection getConnection() {
+//		Connection result = null;
+//
+//		try {
+//			InitialContext ic = new InitialContext();
+//			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+//
+//			result = ds.getConnection();
+//
+//			// Class.forName("org.postgresql.Driver");
+//			// result =
+//			// DriverManager.getConnection("jdbc:postgresql://localhost:5432/ipass",
+//			// "postgres", "postgres");
+//		} catch (Exception ex) {
+//			throw new RuntimeException(ex);
+//		}
+//		return result;
+//	}
+//
+//}
+
 package nl.hu.ipass.webapp;
 
+import java.net.URI;
 import java.sql.Connection;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
 public class PostgresBaseDao {
+	private static DataSource connectionPool;
+
+	public PostgresBaseDao() {
+		if (connectionPool == null) {
+			try {
+				final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
+				if (DATABASE_URL_PROP != null) { // de applicatie draait op Heroku
+					URI dbUri = new URI(DATABASE_URL_PROP);
+					String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+					BasicDataSource pool = new BasicDataSource();
+					if (dbUri.getUserInfo() != null) {
+						pool.setUsername(dbUri.getUserInfo().split(":")[0]);
+						pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+					}
+					pool.setMaxIdle(4); // maximaal 4 ongebruikte connecties
+					pool.setMaxTotal(20); // maximaal 20 connecties in Heroku
+					pool.setDriverClassName("org.postgresql.Driver");
+					pool.setUrl(dbUrl);
+					pool.setInitialSize(1);
+					connectionPool = pool;
+				} else { // de applicatie draait lokaal
+					InitialContext ic = new InitialContext();
+					connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
 	protected final Connection getConnection() {
-		Connection result = null;
-
 		try {
-			InitialContext ic = new InitialContext();
-			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
-
-			result = ds.getConnection();
-
-			// Class.forName("org.postgresql.Driver");
-			// result =
-			// DriverManager.getConnection("jdbc:postgresql://localhost:5432/ipass",
-			// "postgres", "postgres");
+			return connectionPool.getConnection();
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
-		return result;
 	}
-
 }
